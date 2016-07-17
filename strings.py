602351e245c4d5641a44ee225e4196d3db5ddb17
Epidemics Scepticism <epidemics-scepticism@users.noreply.github.com>
2016-07-17 17:32:31 +0000

    Update strings.py
    cleaner, smaller

--- strings.py
+++ strings.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2
-# this code is `strings` in 135bytes (<1 tweet), excluding comments
-import sys,string as s
-p=s.printable[:-4]
+# this code is `strings` in 130bytes (<1 tweet), excluding comments
+import sys,string
 b=''
 for a in open(sys.argv[1]).read():
- if a in p:b+=a;continue;
- if len(b)>3:print(b);b=''
+ if a in string.printable[:-4]:b+=a
+ else:
+  if len(b)>3:print b
+  b=''
