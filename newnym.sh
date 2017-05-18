d320f2c2e9d76878595a39cf2783fe0a8dc8ff8a
Epidemics Scepticism <epidemics-scepticism@users.noreply.github.com>
2017-05-18 03:31:19 +0000

    this script is dead
    b0rk as f0rk

--- newnym.sh
+++ newnym.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 #NEWNYM script for use with Tails in pure bash
+
+# THIS SCRIPT IS BROKEN
+# Tails' new Control Port Filter stops this functioning
+
 exec 3<>/dev/tcp/127.0.0.1/9052;
 if [[ $? -eq 0 ]]; then
 	printf "AUTHENTICATE\r\nSIGNAL NEWNYM\r\nQUIT\r\n" >&3
